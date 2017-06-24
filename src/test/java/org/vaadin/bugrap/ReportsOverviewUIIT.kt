2f3dddbd06cf9402b1d537f2e7d6b2f49aa7d626
sayo-vaadin <sayo@vaadin.com>
2017-06-24 01:15:46 +0100

    Removed UI tests from CI. Eliminated class specification in element selector with reification.

--- src/test/java/org/vaadin/bugrap/ReportsOverviewUIIT.kt
+++ src/test/java/org/vaadin/bugrap/ReportsOverviewUIIT.kt
@@ -19,7 +19,6 @@ import org.vaadin.bugrap.core.ONLY_ME
 import org.vaadin.bugrap.core.OPEN
 import org.vaadin.bugrap.domain.entities.Report.Status.CANT_FIX
 import org.vaadin.bugrap.domain.entities.Report.Status.INVALID
-import kotlin.reflect.KClass
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -45,18 +44,18 @@ class ReportsOverviewUIIT : TestBenchTestCase() {
   @After
   fun teardown() = driver.quit()
 
-  private fun <T : AbstractElement> select(clazz: KClass<T>) = `$`(clazz.java)
+  private inline fun <reified T : AbstractElement> select() = `$`(T::class.java)
 
   @Test
   fun testProjectSelector() {
     println("testProjectSelector")
 
     println("  -> Verify grid only contains reports from selected project")
-    select(NativeSelectElement::class).first().apply {
+    select<NativeSelectElement>().first().apply {
       options.forEach {
         it.text.apply {
           selectByText(this)
-          val projects = select(GridElement::class).first().rows.map { it.getCell(8).text }.distinct()
+          val projects = select<GridElement>().first().rows.map { it.getCell(8).text }.distinct()
           if (projects.isNotEmpty()) {
             assertEquals(1, projects.size)
             assertEquals(this, projects.first())
@@ -73,12 +72,12 @@ class ReportsOverviewUIIT : TestBenchTestCase() {
     println("testVersionSelector")
 
     println("  -> Verfiy version column contains multiple versions")
-    assertTrue(select(GridElement::class).first().rows.map { it.getCell(1).text }.distinct().size > 1)
+    assertTrue(select<GridElement>().first().rows.map { it.getCell(1).text }.distinct().size > 1)
 
     println("  -> Verify version column disappears on version selection")
-    select(NativeSelectElement::class).get(1).apply {
+    select<NativeSelectElement>().get(1).apply {
       selectByText("Version 1")
-      assertEquals("Priority", select(GridElement::class).first().getHeaderCell(0, 1).text)
+      assertEquals("Priority", select<GridElement>().first().getHeaderCell(0, 1).text)
 
       selectByText("All versions")
     }
@@ -89,14 +88,14 @@ class ReportsOverviewUIIT : TestBenchTestCase() {
     println("testSearch")
 
     println("  -> Verify all reports in grid match search term")
-    select(TextFieldElement::class).first().setValue("report")
-    select(GridElement::class).first().rows.map { it.getCell(6).text }.forEach {
+    select<TextFieldElement>().first().setValue("report")
+    select<GridElement>().first().rows.map { it.getCell(6).text }.forEach {
       assertTrue(it.contains("report"))
     }
 
     println("  -> Verify clear search botton clears search field")
-    select(ButtonElement::class).first().click()
-    assertTrue(select(TextFieldElement::class).first().value.isEmpty())
+    select<ButtonElement>().first().click()
+    assertTrue(select<TextFieldElement>().first().value.isEmpty())
   }
 
   @Test
@@ -104,23 +103,23 @@ class ReportsOverviewUIIT : TestBenchTestCase() {
     println("testAssigneeFilter")
 
     println("  -> Verify only \"my reports\" are on grid when \"Only me\" button is clicked")
-    select(ButtonElement::class).caption(ONLY_ME).first().click()
-    var assignees = select(GridElement::class).first().rows.map { it.getCell(4).text }.distinct()
+    select<ButtonElement>().caption(ONLY_ME).first().click()
+    var assignees = select<GridElement>().first().rows.map { it.getCell(4).text }.distinct()
     assertEquals(1, assignees.size)
     assertEquals("developer", assignees.first())
 
     println("  -> Verify \"Only me\" button is visually selected")
-    assertTrue(select(ButtonElement::class).caption(ONLY_ME).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(ButtonElement::class).caption(EVERYONE).first().hasClassName(BUTTON_PRIMARY))
+    assertTrue(select<ButtonElement>().caption(ONLY_ME).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<ButtonElement>().caption(EVERYONE).first().hasClassName(BUTTON_PRIMARY))
 
     println("  -> Verify reports from multiple assignees are on grid when \"Everyone\" button is clicked")
-    select(ButtonElement::class).caption(EVERYONE).first().click()
-    assignees = select(GridElement::class).first().rows.map { it.getCell(4).text }.distinct()
+    select<ButtonElement>().caption(EVERYONE).first().click()
+    assignees = select<GridElement>().first().rows.map { it.getCell(4).text }.distinct()
     assertTrue(assignees.size > 1)
 
     println("  -> Verify \"Only me\" button is visually selected")
-    assertTrue(select(ButtonElement::class).caption(EVERYONE).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(ButtonElement::class).caption(ONLY_ME).first().hasClassName(BUTTON_PRIMARY))
+    assertTrue(select<ButtonElement>().caption(EVERYONE).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<ButtonElement>().caption(ONLY_ME).first().hasClassName(BUTTON_PRIMARY))
   }
 
   @Test
@@ -128,68 +127,72 @@ class ReportsOverviewUIIT : TestBenchTestCase() {
     println("testStatusFilter")
 
     println("  -> Verify only \"open\" are on grid when \"Open\" button is clicked")
-    select(ButtonElement::class).caption(OPEN).first().click()
-    var statuses = select(GridElement::class).first().rows.map { it.getCell(5).text }.distinct()
+    select<ButtonElement>().caption(OPEN).first().click()
+    var statuses = select<GridElement>().first().rows.map { it.getCell(5).text }.distinct()
     println("Statuses: $statuses")
     assertEquals(1, statuses.size)
     assertEquals(OPEN, statuses.first())
 
     println("  -> Verify \"Open\" button is visually selected")
-    assertTrue(select(ButtonElement::class).caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(ButtonElement::class).caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(MenuBarElement::class).first().hasClassName(MENUBAR_THEMED))
+    assertTrue(select<ButtonElement>().caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<ButtonElement>().caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<MenuBarElement>().first().hasClassName(MENUBAR_THEMED))
 
     println("  -> Verify reports with multiple statuses are on grid when \"All kinds\" button is clicked")
-    select(ButtonElement::class).caption(ALL_KINDS).first().click()
-    statuses = select(GridElement::class).first().rows.map { it.getCell(5).text }.distinct()
+    select<ButtonElement>().caption(ALL_KINDS).first().click()
+    statuses = select<GridElement>().first().rows.map { it.getCell(5).text }.distinct()
     assertTrue(statuses.size > 1)
 
     println("  -> Verify \"All kinds\" button is visually selected")
-    assertTrue(select(ButtonElement::class).caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(ButtonElement::class).caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(MenuBarElement::class).first().hasClassName(MENUBAR_THEMED))
+    assertTrue(select<ButtonElement>().caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<ButtonElement>().caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<MenuBarElement>().first().hasClassName(MENUBAR_THEMED))
 
     println("  -> Verify custom filter works")
-    select(MenuBarElement::class).first().click()
-    select(MenuBarElement::class).first().waitForVaadin()
+    select<MenuBarElement>().first().click()
+    select<MenuBarElement>().first().waitForVaadin()
     Thread.sleep(1000) // Horrible hack as the waitForVaadin above did you help matters.
-    select(MenuBarElement::class).first().clickItem(INVALID.toString())
-    statuses = select(GridElement::class).first().rows.map { it.getCell(5).text }.distinct()
+    select<MenuBarElement>().first().clickItem(INVALID.toString())
+    statuses = select<GridElement>().first().rows.map { it.getCell(5).text }.distinct()
     assertEquals(1, statuses.size)
     assertEquals(INVALID.toString(), statuses.first())
 
-    select(MenuBarElement::class).first().click()
-    select(MenuBarElement::class).first().waitForVaadin()
+    select<MenuBarElement>().first().click()
+    select<MenuBarElement>().first().waitForVaadin()
     Thread.sleep(1000) // Horrible hack as the waitForVaadin above did you help matters.
-    select(MenuBarElement::class).first().clickItem(CANT_FIX.toString())
-    select(GridElement::class).first().waitForVaadin()
-    statuses = select(GridElement::class).first().rows.map { it.getCell(5).text }.distinct()
+    select<MenuBarElement>().first().clickItem(CANT_FIX.toString())
+    select<GridElement>().first().waitForVaadin()
+    statuses = select<GridElement>().first().rows.map { it.getCell(5).text }.distinct()
     assertEquals(2, statuses.size)
     assertEquals(setOf(INVALID.toString(), CANT_FIX.toString()), HashSet(statuses))
 
     println("  -> Verify \"Custom\" is visually selected")
-    assertTrue(select(MenuBarElement::class).first().hasClassName(MENUBAR_THEMED))
-    assertFalse(select(ButtonElement::class).caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(ButtonElement::class).caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
+    assertTrue(select<MenuBarElement>().first().hasClassName(MENUBAR_THEMED))
+    assertFalse(select<ButtonElement>().caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<ButtonElement>().caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
 
     println("  -> Verify filter goes back to \"All kinds\" when all custom filters are unselected")
-    select(MenuBarElement::class).first().click()
-    select(MenuBarElement::class).first().waitForVaadin()
+    select<MenuBarElement>().first().click()
+    select<MenuBarElement>().first().waitForVaadin()
     Thread.sleep(1000) // Horrible hack as the waitForVaadin above did you help matters.
-    select(MenuBarElement::class).first().clickItem(INVALID.toString())
-    select(MenuBarElement::class).first().waitForVaadin()
+    select<MenuBarElement>().first().clickItem(INVALID.toString())
+    select<MenuBarElement>().first().waitForVaadin()
     Thread.sleep(1000) // Horrible hack as the waitForVaadin above did you help matters.
-    select(MenuBarElement::class).first().click()
-    select(MenuBarElement::class).first().waitForVaadin()
+    select<MenuBarElement>().first().click()
+    select<MenuBarElement>().first().waitForVaadin()
     Thread.sleep(1000) // Horrible hack as the waitForVaadin above did you help matters.
-    select(MenuBarElement::class).first().clickItem(CANT_FIX.toString())
-    select(MenuBarElement::class).first().waitForVaadin()
-    assertTrue(select(ButtonElement::class).caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(ButtonElement::class).caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
-    assertFalse(select(MenuBarElement::class).first().hasClassName(MENUBAR_THEMED))
+    select<MenuBarElement>().first().clickItem(CANT_FIX.toString())
+    select<MenuBarElement>().first().waitForVaadin()
+    assertTrue(select<ButtonElement>().caption(ALL_KINDS).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<ButtonElement>().caption(OPEN).first().hasClassName(BUTTON_PRIMARY))
+    assertFalse(select<MenuBarElement>().first().hasClassName(MENUBAR_THEMED))
   }
 
+  @Test
   fun testGridSelection() {
+    println("testGridSelection")
+
+    assertTrue(select<GridElement>().exists())
 
   }
 
